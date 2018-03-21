0fa511695b154de7c854167e454f79ae31a70542
aftiani <aftianiwahyu15@gmail.com>
2018-03-21 15:50:28 +0700

    Initial commit

---
+++ app/src/main/java/com/aftiani/linkedln/LinkedInn.java
@@ -0,0 +1,13 @@
+package com.aftiani.linkedln;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinkedInn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linked_inn);
+    }
+}
